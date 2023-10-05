c1a29b158abba46b450cb50e0bc136b7de3a5b31
fahri <fahrisetya733@gmail.com>
2023-10-05 10:49:34 +0700

    mantap lur

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+//navbar item
+const bg = document.querySelector(".rrr");
+window.onscroll = function () {
+  const bottom = window.scrollY;
+  // console.log(top);
+  if (bottom >= 450) {
+    bg.classList.replace("about,mow", "rrr");
+    console.log(bg.classList);
+    bg.classList.replace("rrr", "about,mow");
+  } else {
+    bg.classList.replace("rrr", "about,mow");
+    bg.classList.replace("about,mow", "rrr");
+  }
+};
+
+//penghitung angka
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = "5500";
+
+valueDisplays.forEach((valueDisplays) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplays.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
